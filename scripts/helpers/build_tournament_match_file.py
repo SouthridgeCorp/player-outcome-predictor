79c0ccc239784f7d396c5442da6b4c28e13f47cf
Swati Shekhar <Swati@Swati-Shekhar.local>
2022-10-11 18:12:17 +1100

    Feedback from review - add basic test + move generated data to data/

--- scripts/helpers/build_tournament_match_file.py
+++ scripts/helpers/build_tournament_match_file.py
@@ -1,37 +1,4 @@
-import pandas as pd
-import os
-
-def find_start_date(df, tournament):
-    newdf = df[df["tournament"] == tournament]
-    start_date = newdf['date'].min()
-    return start_date
-
-def find_end_date(df, tournament):
-    newdf = df[df["tournament"] == tournament]
-    end_date = newdf['date'].max()
-    return end_date
-
-def read_all_matches():
-    base_dir = "resources/match_data"
-    if not os.path.isdir(base_dir):
-        os.mkdir(base_dir)
-
-    input_file = "scripts/helpers/resources/IPLMatches.csv"
-    all_matches = pd.read_csv(input_file)
-
-    # Associate the tournament with the matches
-    all_matches['tournament'] = all_matches['date'].apply(lambda x: "IPL-{}".format(x[-4:]))
-    all_matches['date'] = pd.to_datetime(all_matches['date'], format='%d/%m/%Y')
-    matches_file = "{}/matches.csv".format(base_dir)
-    all_matches.to_csv(matches_file, index = False)
-
-    # Set a start & end date of the tournament from the list of first & last matches
-    tournaments = pd.DataFrame(all_matches['tournament'].unique(), columns=['tournament'])
-    tournaments['start_date'] = [find_start_date(all_matches, x) for x in tournaments['tournament']]
-    tournaments['end_date'] = [find_end_date(all_matches, x) for x in tournaments['tournament']]
-
-    tournament_file = "{}/tournaments.csv".format(base_dir)
-    tournaments.to_csv(tournament_file, index=False)
+import helper_utils
 
 
-read_all_matches()
+helper_utils.read_all_matches()
--- utils/match_utils/data_reader_utils.py
+++ utils/match_utils/data_reader_utils.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
-MATCH_FILE_NAME = "resources/match_data/matches.csv"
-TOURNAMENT_FILE_NAME = "resources/match_data/tournaments.csv"
+# TODO: Move to an external config file
+MATCH_FILE_NAME = "data/generated/match_data/matches.csv"
+TOURNAMENT_FILE_NAME = "data/generated/match_data/tournaments.csv"
 
 
 class Tournaments:
@@ -11,10 +12,16 @@ class Tournaments:
     '''
     def __init__(self, tournament_file_name=TOURNAMENT_FILE_NAME):
         self.df = pd.read_csv(tournament_file_name)
-        self.training_start = self.df['start_date'][1]
-        self.training_end = self.df['start_date'][3]
-        self.testing_start = self.df['start_date'][3]
-        self.testing_end = self.df['start_date'][6]
+        if len(self.df['start_date']) >= 6:
+            self.training_start = self.df['start_date'][0]
+            self.training_end = self.df['start_date'][3]
+            self.testing_start = self.df['start_date'][3]
+            self.testing_end = self.df['start_date'][6]
+        else:
+            self.training_start = self.df['start_date'][0]
+            self.training_end = self.df['start_date'][0]
+            self.testing_start = self.df['start_date'][0]
+            self.testing_end = self.df['start_date'][0]
 
 
 class Matches:
@@ -23,4 +30,4 @@ class Matches:
         Initialising the class reads the content from the tournament file
     '''
     def __init__(self, match_file_name = MATCH_FILE_NAME):
-        self.match_df = pd.read_csv(match_file_name)
+        self.df = pd.read_csv(match_file_name)
