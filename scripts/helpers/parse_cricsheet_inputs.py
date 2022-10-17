962b4b0fa0396304c0fd4bc8e6672590ffabe041
Swati Shekhar <Swati@Swati-Shekhar.local>
2022-10-18 10:03:55 +1100

    Self review updates

--- scripts/helpers/parse_cricsheet_inputs.py
+++ scripts/helpers/parse_cricsheet_inputs.py
@@ -1,31 +1,50 @@
 import os
 import pandas as pd
 import json
+import logging
 
-# Note: PLEASE READ README_parse_cricsheet_inputs.md before running this file.
-#
-
-
+# TODO: Fields to be parameterized
 BASE_INPUT_DIR = "data/downloaded/cricsheet/"
 BASE_OUTPUT_DIR = "data/generated/match_data/cricsheet/"
+logging.basicConfig(level=logging.DEBUG)
+
 
 class PlayerMap:
+    """
+    Utility class to help parse / update global player information.
+    """
     def __init__(self):
         self.number_of_players = 0
+        # This is a dict of dicts, with key = player key and value = dict of all player attributes known
         self.player_dict = {}
+        # This is a perf optimisation, since the cricsheet data refers to player names and we need a quick way to
+        # find the key for a player name
         self.player_name_reverse_map = {}
 
     def find_key_for_player_name(self, player_name):
+        """
+        Returns the key for a player name. Builds it out if it doesn't exist yet in the reverse map.
+        :param player_name: The player name to look up
+        :return: The key corresponding to the player name
+        """
         if player_name in self.player_name_reverse_map.keys():
             return self.player_name_reverse_map[player_name]
+
+        # Building out the keys
         split_names = player_name.split()
         key = ""
-        for name_substr in split_names:
-            key += name_substr[0]
+        for name_substring in split_names:
+            key += name_substring[0]
         key += player_name[-1] + str(self.number_of_players)
         return key
 
     def insert_player_information(self, player_name, player_key=None):
+        """
+        Inserts player information into the map, and returns the key corresponding to the player.
+        :param player_name: The name of the player to insert
+        :param player_key: The player key to map the player too. If the key is None, a new entry is created.
+        :return: The key corresponding to this entry
+        """
         if player_key is None:
             key = self.find_key_for_player_name(player_name)
         else:
@@ -38,6 +57,10 @@ class PlayerMap:
         return key
 
     def get_dataframe(self):
+        """
+        Return the contents of this map as a pandas dataframe
+        :return: The dataframe containing all the contents of this map
+        """
         player_list = []
 
         if self.number_of_players == 0:
@@ -51,11 +74,9 @@ class PlayerMap:
         return player_df
 
 
-
-
 def set_json_value_if_exists(json_object, target_dict, key_in_json, key_in_dict, index=-1,
                              is_player=False, player_map=None):
-    '''
+    """
     Helper function which copies the specified key from the json object if the key exists, and sets it as a value
     in the target dictionary. If the key does not exist in the json object, then it sets a default value in the target
     dictionary.
@@ -64,8 +85,11 @@ def set_json_value_if_exists(json_object, target_dict, key_in_json, key_in_dict,
     :param key_in_json: The name of the key to look for in the json object
     :param key_in_dict: The name of the key to set the value for in the tagret dictionary
     :param index: If index > -1, the function treats the json object as a list of jsons and queries the specified index
-    :return:
-    '''
+    :param is_player: If True, then the function will look to update the player_map with the values
+    :param player_map: The global map of all players - must be defined if is_player=True. This map will be updated with
+    new player information discovered.
+    :return: None
+    """
     if key_in_json in json_object.keys():
         if index == -1:
             value = json_object[key_in_json]
@@ -79,28 +103,33 @@ def set_json_value_if_exists(json_object, target_dict, key_in_json, key_in_dict,
     else:
         target_dict[key_in_dict] = ""
 
+
 def parse_player_data(player_json_list, team, match_key, player_info, player_map):
-    '''
+    """
     Parses the list of players from the input json list and sets the player information to be copied over
     :param player_json_list: The list of players to look through
     :param team: The team for which the players are enumerated
     :param match_key: The key for the match, to be included in the target list
     :param player_info: The target player list which is updated with the player information as a hash-map
-    :return:
-    '''
+    :param player_map: The global map of all players. This map will be updated with new player information discovered
+    as the matches are parsed.
+    :return: None
+    """
     for player in player_json_list[team]:
-        player_key = player_map.insert_player_information(player_name = player)
+        player_key = player_map.insert_player_information(player_name=player)
         player_info.append({"team": team, "match_key": match_key, "player_key": player_key})
 
 
 def parse_innings_data(json_innings, match_key, innings_list, player_map):
-    '''
+    """
     Parses the innings object in the cricsheet json, and populates the innings list with a map of all related fields
     :param json_innings: The json innings object to parse
     :param match_key: The key of the match associated with this innings
     :param innings_list: The list of innings to populate
-    :return:
-    '''
+    :param player_map: The global map of all players. This map will be updated with new player information discovered
+    as the matches are parsed.
+    :return: None
+    """
     inning_count = 0
     for inning in json_innings:
         inning_count += 1  # Ensure we keep track of the innings number
@@ -108,7 +137,6 @@ def parse_innings_data(json_innings, match_key, innings_list, player_map):
         over_count = -1
         for over_row in inning["overs"]:
             over_count += 1  # Ensure we keep track of the over number
-            over_number = over_row["over"]
             ball_number = 0
 
             for delivery in over_row["deliveries"]:
@@ -172,6 +200,8 @@ def parse_json_match_data(input_file, tournament_key, match_dict_list, playing_x
     :param match_dict_list: The list to append with the match details (as a map)
     :param playing_xi_dict_list: The list to append with the player details (as a map)
     :param innings_list: The list to append with the innings details (as a map)
+    :param player_map: The global map of all players. This map will be updated with new player information discovered
+    as the matches are parsed.
     :return: None
     """
     with open(input_file, 'r') as json_file:
@@ -222,13 +252,15 @@ def parse_json_match_data(input_file, tournament_key, match_dict_list, playing_x
         if "innings" in json_object.keys():
             parse_innings_data(json_object["innings"], match_dict["key"], innings_list, player_map)
     else:
-        print("Ignoring match gender:{} type:{}".format(gender, match_type))
+        logging.debug(f"Ignoring match gender:{gender} type:{match_type}")
 
 
 def parse_match_data(tournament_key, player_map):
     """
     Goes through all the match JSONs associated with the tournament and creates the dataframes for matches, players and
     innings.
+    :param player_map: The global map of all players. This map will be updated with new player information discovered
+    as the matches are parsed.
     :param tournament_key: The key of the tournament
     :return: None
     """
@@ -242,7 +274,7 @@ def parse_match_data(tournament_key, player_map):
             parse_json_match_data(filename.path, tournament_key, match_dict_list, playing_xi_dict_list, innings_list,
                                   player_map)
         else:
-            print("Skipping non-json file {}".format(filename.path))
+            logging.debug(f"Skipping non-json file {filename.path}")
 
     match_df = pd.DataFrame(data=match_dict_list)
     playing_xi_df = pd.DataFrame(data=playing_xi_dict_list)
@@ -278,7 +310,7 @@ def write_tournament_data(tournament_key, tournament_name, tournament_file, tour
     :param tournament_file: The file to write to
     :param tournament_start: The date of the first match in the tournament series
     :param tournament_end: The date of the last match in the tournament series
-    :return:
+    :return: None
     """
     if not os.path.exists(tournament_file):
         column_list = ["key", "name", "first_match_date", "last_match_date"]
@@ -299,6 +331,13 @@ def write_tournament_data(tournament_key, tournament_name, tournament_file, tour
 
 
 def read_players_file(player_file, player_map):
+    """
+    Reads the global players file and builds out a map of all the information retrieved. This map can be updated by
+    other data points and then finally be used to rewrite the cricsheet file.
+    :param player_file: The full file path for player information
+    :param player_map: The map which should be updated with the information read so far
+    :return: None
+    """
     if os.path.exists(player_file):
         player_df = pd.read_csv(player_file)
         player_list = player_df.to_dict('records')
@@ -307,30 +346,43 @@ def read_players_file(player_file, player_map):
 
 
 def write_players_file(player_file, player_map):
+    """
+    Write the global player file
+    :param player_file: The full file path for player information
+    :param player_map: Details of the players to be documented
+    :return: None
+    """
     player_df = player_map.get_dataframe()
     player_df.to_csv(player_file, index=False)
 
 
 def parse_data(tournament_key, tournament_name):
     """
-    Parses & writes match & tournament data
+    Parses & writes match, tournament and player data
     :param tournament_key: The key of the tournament
     :param tournament_name: The full name of the tournament
     :return: None
     """
 
-    tournament_file = BASE_OUTPUT_DIR + "tournaments.csv"
+    logging.info(f"Parsing ({tournament_key}) {tournament_name}")
+    # read all details already available in the global players file
     player_file = BASE_OUTPUT_DIR + "players.csv"
-
     player_map = PlayerMap()
     read_players_file(player_file, player_map)
+
+    # parse the matches json and build out per match meta-data, playing xi data & innings data.
     match_df, playing_xi_df, innings_df = parse_match_data(tournament_key, player_map)
+
+    # Write per match data into the tournament specific directory
     write_match_data(tournament_key, match_df, playing_xi_df, innings_df)
 
-    write_tournament_data(tournament_key, tournament_name, tournament_file,
-                          match_df["date"].min(), match_df["date"].max())
-
+    # Write global player data
     write_players_file(player_file, player_map)
+
+    # Write global tournament data
+    tournament_file = BASE_OUTPUT_DIR + "tournaments.csv"
+    write_tournament_data(tournament_key, tournament_name, tournament_file, match_df["date"].min(),
+                          match_df["date"].max())
 
 
 def main():
