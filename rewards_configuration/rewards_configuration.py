63b96101c8c369ab7d3b9abbbfd20ca2d91558e1
Swati Shekhar <Swati@Swati-Shekhar.local>
2022-10-25 12:19:12 +1100

    Rebase - First draft of the changes - not yet completed

--- rewards_configuration/rewards_configuration.py
+++ rewards_configuration/rewards_configuration.py
@@ -1,11 +1,10 @@
 from utils.config_utils import ConfigUtils
 import pandas as pd
 
+
 class RewardsConfiguration:
 
-    def __init__(self, static_data_config:ConfigUtils):
-        #TODO: Instantiate the class by setting class attributes for variable in the
+    def __init__(self, static_data_config: ConfigUtils):
+        # TODO: Instantiate the class by setting class attributes for variable in the
         # rewards_formula_calculation_parameters subgraph of the computational model
         pass
-
-
