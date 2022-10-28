91e4fac60ab890106a3b5a565c9162ca887c911a
Swati Shekhar <Swati@Swati-Shekhar.local>
2022-10-28 15:09:10 +1100

    Added comments

--- historical_data/players.py
+++ historical_data/players.py
@@ -9,7 +9,13 @@ class Players:
         self.players_df = pd.read_csv(player_file)
         self.players_df.set_index("key")
 
-    def merge_with_players(self, source_df, source_key) -> pd.DataFrame:
+    def merge_with_players(self, source_df: pd.DataFrame, source_key: str) -> pd.DataFrame:
+        """
+        Merges the player details with the source column to create a combined dataframe
+        :param source_df: The dataframe to merge with
+        :param source_key: The key for the source df
+        :return: The merged df
+        """
         df = pd.merge(self.players_df, source_df, left_on="key", right_on=source_key)
         df.drop('key', axis=1, inplace=True)
         return df
