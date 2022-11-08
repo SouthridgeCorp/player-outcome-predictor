c6596bf1a512cf4b5d1c1d3bc6779ad050b6c9c5
Gireesh Ramji <gireesh@fractal-data.com>
2022-11-08 22:15:44 +1100

    issue#28: temp commit with some test scaffolding

--- test/inferential_models/bowling_outcomes_models/test_first_innings_by_ball_model.py
+++ test/inferential_models/bowling_outcomes_models/test_first_innings_by_ball_model.py
@@ -2,9 +2,12 @@ import pandas as pd
 import pytest
 import os
 import random
-from inferential_models.bowling_outcomes_models.first_innings_by_ball_model import sample_model
+from inferential_models.bowling_outcomes_models.first_innings_by_ball_model import sample_model, real_model
 import pymc as pm
 import aesara.tensor as at
+from test.conftest import get_test_cases
+from test.simulators.conftest import perfect_simulator
+from test.data_selection.conftest import setup_training_and_testing
 
 def test_model(simulate_match_data):
     bowling_outcomes_by_ball_and_innings_df, \
@@ -55,3 +58,36 @@ def test_dummy_model(simulate_fake_data):
             extend_inferencedata=True,
             random_seed=RANDOM_SEED,
         )
+
+def get_innings_data(bowling_outcomes_df, match_state_df, inning):
+    bowling_outcomes_innings_df = bowling_outcomes_df.query(f'inning == {inning}')
+    match_state_innings_df = match_state_df.query(f'inning == {inning}')
+    return bowling_outcomes_innings_df, match_state_innings_df
+
+@pytest.mark.parametrize(
+    'test_case',
+    get_test_cases('app_config', 'TestPerfectSimulator'),
+    scope='class'
+)
+class TestFirstInningsModel:
+
+    def test_real_model(self,perfect_simulator):
+        setup_training_and_testing(perfect_simulator.data_selection, False)
+        train_bowling_outcomes_by_ball_and_innings_df = perfect_simulator.get_bowling_outcomes_by_ball_and_innings(False)
+        train_match_state_df = perfect_simulator.get_match_state_by_ball_and_innings(False)
+        train_first_innings_dfs = get_innings_data(train_bowling_outcomes_by_ball_and_innings_df,
+                                             train_match_state_df,
+                                             inning=1)
+
+
+        test_bowling_outcomes_by_ball_and_innings_df = perfect_simulator.get_bowling_outcomes_by_ball_and_innings(True)
+        test_match_state_df = perfect_simulator.get_match_state_by_ball_and_innings(True)
+        test_first_innings_dfs = get_innings_data(test_bowling_outcomes_by_ball_and_innings_df,
+                                             test_match_state_df,
+                                             inning=1)
+        real_model(train_first_innings_dfs[0],
+                   train_first_innings_dfs[1])
+        real_model(test_first_innings_dfs[0],
+                   test_first_innings_dfs[1])
+        assert False
+
