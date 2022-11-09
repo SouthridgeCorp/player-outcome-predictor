31cfebad0ab9a1e258e773f56dbd743b04e566df
Swati Shekhar <Swati@Swati-Shekhar.local>
2022-11-09 14:35:35 +1100

    Self review #1

--- data_selection/data_selection.py
+++ data_selection/data_selection.py
@@ -1,8 +1,6 @@
 from historical_data.singleton import Helper
 import pandas as pd
 from historical_data.playing_xi import PlayingXI
-import warnings
-warnings.filterwarnings('ignore')
 
 
 class PlayerInformation:
